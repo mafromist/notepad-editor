f4308f16af5e53d32822ddcb831b8573ba487075
mafromist <mafromist@gmail.com>
2024-08-23 00:05:16 +0100

    downgraded to react 17

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import react from 'react'
+import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import './index.css'
 
 // Use your environment variables in the code
 
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.render(<App/>, document.getElementById('root'))
+
