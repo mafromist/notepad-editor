e6edde00be3b7a7ddca4622c056f20b4f8f91f47
mafromist <mafromist@gmail.com>
2024-08-22 15:32:59 +0100

    created env

--- firebase.js
+++ firebase.js
@@ -1,18 +1,28 @@
 // Import the functions you need from the SDKs you need
 import { collection, getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const firebaseApiKey = process.env.FIREBASE_API_KEY;
+const firebaseAuthDomain = process.env.FIREBASE_AUTH_DOMAIN;
+const firebaseProjectId = process.env.FIREBASE_PROJECT_ID;
+const firebaseStorageBucket = process.env.FIREBASE_STORAGE_BUCKET;
+const firebaseMessagingSenderId = process.env.FIREBASE_MESSAGING_SENDER_ID;
+const firebaseAppId = process.env.FIREBASE_APP_ID;
+const firebaseDatabaseURL = process.env.FIREBASE_DATABASE_URL;
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAUlYv7HwfBscR1sYT18e6hDOLgk3YNMac",
-  authDomain: "react-scrimba-notepad.firebaseapp.com",
-  projectId: "react-scrimba-notepad",
-  storageBucket: "react-scrimba-notepad.appspot.com",
-  messagingSenderId: "982759502726",
-  appId: "1:982759502726:web:6630384ec5874b1b7e7207",
-  databaseURL: "https://react-scrimba-notepad.firebaseio.com"
+  apiKey: firebaseApiKey,
+  authDomain: firebaseAuthDomain,
+  projectId: firebaseProjectId,
+  storageBucket: firebaseStorageBucket,
+  messagingSenderId: firebaseMessagingSenderId,
+  appId: firebaseAppId,
+  databaseURL: firebaseDatabaseURL
 };
 
 // Initialize Firebase
@@ -20,3 +30,12 @@ const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app)
 export const notesCollection = collection(db, "notes")
+
+
+FIREBASE_API_KEY="AIzaSyAUlYv7HwfBscR1sYT18e6hDOLgk3YNMac"
+FIREBASE_AUTH_DOMAIN="react-scrimba-notepad.firebaseapp.com"
+FIREBASE_PROJECT_ID="react-scrimba-notepad"
+FIREBASE_STORAGE_BUCKET="react-scrimba-notepad.appspot.com"
+FIREBASE_MESSAGING_SENDER_ID="982759502726",
+FIREBASE_APP_ID="1:982759502726:web:6630384ec5874b1b7e7207"
+FIREBASE_DATABASE_URL="https://react-scrimba-notepad.firebaseio.com"
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Use your environment variables in the code
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
