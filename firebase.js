7e072cc3c485611959a3a49ca48fa598318fce8f
mafromist <mafromist@gmail.com>
2024-08-22 15:42:32 +0100

    updated variables on env

--- firebase.js
+++ firebase.js
@@ -2,13 +2,14 @@
 import { collection, getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 
-const firebaseApiKey = import.meta.env.FIREBASE_API_KEY;
-const firebaseAuthDomain = import.meta.env.FIREBASE_AUTH_DOMAIN;
-const firebaseProjectId = import.meta.env.FIREBASE_PROJECT_ID;
-const firebaseStorageBucket = import.meta.env.FIREBASE_STORAGE_BUCKET;
-const firebaseMessagingSenderId = import.meta.env.FIREBASE_MESSAGING_SENDER_ID;
-const firebaseAppId = import.meta.env.FIREBASE_APP_ID;
-const firebaseDatabaseURL = import.meta.env.FIREBASE_DATABASE_URL;
+const firebaseApiKey = import.meta.env.VITE_FIREBASE_API_KEY;
+console.log({firebaseApiKey})
+const firebaseAuthDomain = import.meta.env.VITE_FIREBASE_AUTH_DOMAIN;
+const firebaseProjectId = import.meta.env.VITE_FIREBASE_PROJECT_ID;
+const firebaseStorageBucket = import.meta.env.VITE_FIREBASE_STORAGE_BUCKET;
+const firebaseMessagingSenderId = import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID;
+const firebaseAppId = import.meta.env.VITE_FIREBASE_APP_ID;
+const firebaseDatabaseURL = import.meta.env.VITE_FIREBASE_DATABASE_URL;
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
